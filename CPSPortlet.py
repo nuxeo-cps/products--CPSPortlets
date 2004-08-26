f32791a469cdad8c13e8b619a03026a84329c1dd
janguenot <devnull@localhost>
2004-08-26 15:20:45 +0000

    Base CPS Portlet Class (inherits from CPSDocument)

---
+++ CPSPortlet.py
@@ -0,0 +1,54 @@
+# -*- coding: ISO-8859-15 -*-
+# Copyright (c) 2004 Nuxeo SARL <http://nuxeo.com>
+# Author : Julien Anguenot <ja@nuxeo.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# $Id$
+
+__author__ = "Julien Anguenot <mailto:ja@nuxeo.com>"
+
+"""CPS Portlet
+
+This is a CPSDocument child base class for portlets
+"""
+
+from Globals import InitializeClass
+
+from Products.CPSDocument.CPSDocument import CPSDocument
+
+class CPSPortlet(CPSDocument):
+    """ CPS Portlet
+
+    This is a CPSDocument child base class for portlets
+    """
+
+    meta_type = 'CPS Portlet'
+    portal_type = meta_type
+
+InitializeClass(CPSPortlet)
+
+def addCPSPortlet(container, id, REQUEST=None, **kw):
+    """Add a bare CPS Portlet.
+
+    The object doesn't have a portal_type yet, so we have no way to know
+    its schema. This simply constructs a bare instance.
+    """
+    ob = CPSPortlet(id, **kw)
+    container._setObject(id, ob)
+    if REQUEST:
+        ob = container._getOb(id)
+        REQUEST.RESPONSE.redirect(ob.absolute_url()+'/manage_main')
+
+
