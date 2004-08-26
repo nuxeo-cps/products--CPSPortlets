acb6449e80b3331fad39ebb66ab5e8c60b5e7a83
janguenot <devnull@localhost>
2004-08-26 18:28:38 +0000

    Widget declaration

---
+++ skins/cpsportlets_widgets/getPortletWidgets.py
@@ -0,0 +1,12 @@
+##parameters=
+"""Return portlet widgets
+"""
+
+widgets = {
+    'CPS Portlet Dummy Widget': {
+    'type': 'CPS Portlet Dummy Widget Type',
+    'data': {},
+    }
+    }
+
+return widgets
