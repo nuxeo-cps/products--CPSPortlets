be787d1a7dc593e6aed9d513537837241cafd425
janguenot <devnull@localhost>
2004-09-04 21:59:00 +0000

    Add an identifier to the portlet. As well, all the fields must be searchable

--- skins/cpsportlets_schemas/getPortletCommonSchema.py
+++ skins/cpsportlets_schemas/getPortletCommonSchema.py
@@ -3,11 +3,29 @@
 """
 
 portlet_common_schema = {
+    'identifier': {
+        'type': 'CPS String Field',
+        'data': {
+            'default_expr': 'string:',
+            'is_searchabletext': 1,
+            'acl_read_permissions': '',
+            'acl_read_roles': '',
+            'acl_read_expr': '',
+            'acl_write_permissions': '',
+            'acl_write_roles': '',
+            'acl_write_expr': '',
+            'read_ignore_storage': 0,
+            'read_process_expr': '',
+            'read_process_dependent_fields': [],
+            'write_ignore_storage': 0,
+            'write_process_expr': '',
+        },
+    },
     'provider': {
         'type': 'CPS String Field',
         'data': {
             'default_expr': 'string:',
-            'is_searchabletext': 0,
+            'is_searchabletext': 1,
             'acl_read_permissions': '',
             'acl_read_roles': '',
             'acl_read_expr': '',
@@ -25,7 +43,7 @@ portlet_common_schema = {
         'type': 'CPS String Field',
         'data': {
             'default_expr': 'string:',
-            'is_searchabletext': 0,
+            'is_searchabletext': 1,
             'acl_read_permissions': '',
             'acl_read_roles': '',
             'acl_read_expr': '',
@@ -42,8 +60,8 @@ portlet_common_schema = {
     'state': {
         'type': 'CPS String Field',
         'data': {
-            'default_expr': 'string:',
-            'is_searchabletext': 0,
+            'default_expr': 'python:0',
+            'is_searchabletext': 1,
             'acl_read_permissions': '',
             'acl_read_roles': '',
             'acl_read_expr': '',
@@ -61,7 +79,7 @@ portlet_common_schema = {
         'type': 'CPS Int Field',
         'data': {
             'default_expr': 'python:0',
-            'is_searchabletext': 0,
+            'is_searchabletext': 1,
             'acl_read_permissions': '',
             'acl_read_roles': '',
             'acl_read_expr': '',
