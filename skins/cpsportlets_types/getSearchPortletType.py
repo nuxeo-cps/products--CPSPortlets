56671efb72f95cfc25c982661eebb9823eb66d2b
janguenot <devnull@localhost>
2004-09-06 01:02:31 +0000

    Type declaration for the Search Portlet

---
+++ skins/cpsportlets_types/getSearchPortletType.py
@@ -0,0 +1,48 @@
+search_portlet_type = {
+    'title': 'portal_type_SearchPortlet_title',
+    'description': '',
+    'content_icon': 'portlet_icon.gif',
+    'content_meta_type': 'CPS Portlet',
+    'product': 'CPSPortlets',
+    'factory': 'addCPSPortlet',
+    'immediate_view': 'cpsportlet_view',
+    'global_allow': True,
+    'filter_content_types': True,
+    'allowed_content_types': (),
+    'allow_discussion': False,
+    'cps_is_searchable': True,
+    'cps_proxy_type': '',
+    'cps_display_as_document_in_listing': False,
+    'schemas': ('portlet_common', 'search_portlet'),
+    'layouts': ('search_portlet',),
+    'flexible_layouts': (),
+    'storage_methods': (),
+    'cps_is_portlet': True,
+    'actions': (
+         {'id': 'create',
+          'name': 'action_create',
+          'action': 'string:${object_url}/cpsportlet_create_form',
+          'condition': '',
+          'permission': ('Manage Portlets',),
+          'category': 'object',
+          'visible': 0,},
+         {'id': 'edit',
+          'name': 'action_edit',
+          'action': 'string:${object_url}/cpsportlet_edit_form',
+          'condition': '',
+          'permission': ('Manage Portlets',),
+          'category': 'object',
+          'visible': 0,},
+         {'id': 'canvas',
+          'name': 'action_canvas',
+          'action': "python: object.getLocalFolder().absolute_url() + '/portlet_manage_form'",
+          'condition': '',
+          'permission': ('Manage Portlets',),
+          'category': 'object',
+          'visible': 1,},
+    )
+}
+
+types = {'Search Portlet': search_portlet_type}
+
+return types
