0ac8d0a92dca142059118ba2d1f0762c7c07c4e9
janguenot <devnull@localhost>
2004-09-06 01:02:57 +0000

    Register new widget type

--- skins/cpsportlets_widgets/getPortletWidgets.py
+++ skins/cpsportlets_widgets/getPortletWidgets.py
@@ -6,7 +6,11 @@ widgets = {
     'CPS Portlet Dummy Widget': {
     'type': 'CPS Portlet Dummy Widget Type',
     'data': {},
-    }
+    },
+    'CPS Search Widget': {
+    'type': 'CPS Search Widget Type',
+    'data': {},
+    },
     }
 
 return widgets
