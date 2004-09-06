5695cbfbf6b7b9b35b28cf8aa5d4262b8f697824
janguenot <devnull@localhost>
2004-09-06 01:01:57 +0000

    Layout for the SearchPortlet

---
+++ skins/cpsportlets_layouts/getSearchPortletLayout.py
@@ -0,0 +1,36 @@
+search_portlet_layout = {
+    'widgets': {
+        'search': {
+            'type': 'CPS Search Widget',
+            'data': {
+                'title': '',
+                'fields': ('dummy',),
+                'is_required': True,
+                'label': '',
+                'label_edit': '',
+                'description': '',
+                'help': '',
+                'is_i18n': False,
+                'readonly_layout_modes': (),
+                'hidden_layout_modes': (),
+                'hidden_readonly_layout_modes': (),
+                'hidden_empty': False,
+                'hidden_if_expr': '',
+                'css_class': '',
+                'widget_mode_expr': '',
+            },
+        },
+    },
+    'layout': {
+        'style_prefix': 'layout_default_',
+        'flexible_widgets': (),
+        'ncols': 1,
+        'rows': [
+            [{'widget_id': 'search', 'ncols': 1},
+            ],
+        ],
+    },
+}
+
+layouts = {'search_portlet':search_portlet_layout}
+return layouts
