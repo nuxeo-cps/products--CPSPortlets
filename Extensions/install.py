2bc297e3405be7ba7733cdfc6886cbce49b4c735
jmorliaguet <devnull@localhost>
2004-09-16 20:23:20 +0000

    initial support for CMF widgets.
    - CPS3 specific skins are located in skins/cpsportlets_widgets_cps3
    - CMF skins are located in skins/cpsportlets_widgets
    
    to install CPSPortlets on CMF comment out the lines:
    
    # CPS3 specific ---------------------------------------------
             'cpsportlets_widgets_cps3':
             'Products/CPSPortlets/skins/cpsportlets_widgets_cps3',
    # -----------------------------------------------------------
    
    in Extensions/install.py

--- Extensions/install.py
+++ Extensions/install.py
@@ -35,6 +35,10 @@ SECTIONS_ID = 'sections'
 WORKSPACES_ID = 'workspaces'
 SKINS = {'cpsportlets_widgets':
          'Products/CPSPortlets/skins/cpsportlets_widgets',
+# CPS3 specific ---------------------------------------------
+         'cpsportlets_widgets_cps3':
+         'Products/CPSPortlets/skins/cpsportlets_widgets_cps3',
+# -----------------------------------------------------------
          'cpsportlets_default':
          'Products/CPSPortlets/skins/cpsportlets_default',
          'cpsportlets_schemas':
---
+++ skins/cpsportlets_widgets_cps3/getCreatableItems.py
@@ -0,0 +1,1 @@
+return context.getSortedContentTypes(allowed=1)
