4130fd2a5c234a50fd4e8ee116d5894efb8663d5
jmorliaguet <devnull@localhost>
2004-09-16 23:35:23 +0000

    fixed a typo and added an optional link to the advanced search form

--- skins/cpsportlets_layouts/getSearchPortletLayout.py
+++ skins/cpsportlets_layouts/getSearchPortletLayout.py
@@ -20,6 +20,28 @@ search_portlet_layout = {
                 'widget_mode_expr': '',
             },
         },
+        'advanced_search_link': {
+            'type': 'CheckBox Widget',
+            'data': {
+                'title': '',
+                'fields': ('advanced_search_link',),
+                'is_required': False,
+                'label': '',
+                'label_edit': 'cpsportlets_advanced_search_link_label',
+                'description': '',
+                'help': '',
+                'is_i18n': False,
+                'readonly_layout_modes': (),
+                'hidden_layout_modes': ('view',),
+                'hidden_readonly_layout_modes': (),
+                'hidden_empty': False,
+                'hidden_if_expr': '',
+                'css_class': '',
+                'widget_mode_expr': '',
+                'display_true': 'Yes',
+                'display_false': 'No',
+            },
+        },
     },
     'layout': {
         'style_prefix': 'layout_default_',
@@ -28,9 +50,12 @@ search_portlet_layout = {
         'rows': [
             [{'widget_id': 'search', 'ncols': 1},
             ],
+            [{'widget_id': 'advanced_search_link', 'ncols': 1},
+            ],
         ],
     },
 }
+
 
 layouts = {'search_portlet':search_portlet_layout}
 return layouts
--- skins/cpsportlets_schemas/getSearchPortletSchema.py
+++ skins/cpsportlets_schemas/getSearchPortletSchema.py
@@ -1,3 +1,4 @@
+
 search_portlet_schema = {
     'dummy': {
         'type': 'CPS String Field',
@@ -17,8 +18,25 @@ search_portlet_schema = {
             'write_process_expr': '',
         },
     },
+    'advanced_search_link': {
+        'type': 'CPS Int Field',
+        'data': {
+            'default_expr': 'python:0',
+            'is_searchabletext': 0,
+            'acl_read_permissions': '',
+            'acl_read_roles': '',
+            'acl_read_expr': '',
+            'acl_write_permissions': '',
+            'acl_write_roles': '',
+            'acl_write_expr': '',
+            'read_ignore_storage': 0,
+            'read_process_expr': '',
+            'read_process_dependent_fields': [],
+            'write_ignore_storage': 0,
+            'write_process_expr': '',
+        },
+    },
 }
 
 schemas = {'search_portlet': search_portlet_schema}
-
 return schemas
