b779e78e59400d9d54a8d5d507ad8305b011e2b9
jmorliaguet <devnull@localhost>
2004-10-28 14:23:57 +0000

    added an option to the RSS portlet to set the maximum number of words in the summary.

--- skins/cpsportlets_widgets_cps3/getRSSItems.py
+++ skins/cpsportlets_widgets_cps3/getRSSItems.py
@@ -1,4 +1,4 @@
-##parameters=channel, max_items=None
+##parameters=channel, max_items=0, max_words=0
 
 channel = getattr(context.portal_rss, channel, None)
 
@@ -6,4 +6,27 @@ if channel is None:
     return []
 
 data = channel.getData(max_items)
-return data['lines']
+
+items = []
+
+data_items = data['lines']
+if max_words == 0:
+    return data_items
+
+def summarize(text='', max_words=20):
+    """summarize the text by returning the first max_words
+    """
+    split_text = text.split(' ', max_words)[0:max_words]
+    res = ''
+    if split_text:
+        res = ' '.join(split_text) + ' ...'
+    return res
+
+# summarize the descriptions
+pos = 0
+for item in data_items:
+    description = summarize(item['description'], max_words)
+    data_items[pos].update({'description': description})
+    pos += 1
+
+return data_items
