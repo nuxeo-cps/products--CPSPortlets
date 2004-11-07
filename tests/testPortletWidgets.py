986030b197a29342fc00ace0dc6b03f427410888
jmorliaguet <devnull@localhost>
2004-11-07 17:30:01 +0000

    running 'portlet.render()' without assert_ since the result can be an empty string with the 'layout_portlet_' layout

--- tests/testPortletWidgets.py
+++ tests/testPortletWidgets.py
@@ -27,7 +27,7 @@ class TestPortlet(TestPortlets):
         portlet_id = ptltool.createPortlet(ptype_id)
         self.assert_(len(ptltool.items()) == 1)
         portlet = ptltool[portlet_id]
-        self.assert_(portlet.render())
+        portlet.render()
         self.assert_(portlet.render_js() is not None)
 
 # portal type list
