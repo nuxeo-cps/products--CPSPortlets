3c28b6ea82e3010afa6d7b582ed9b1be066dcec7
jmorliaguet <devnull@localhost>
2004-11-14 20:16:06 +0000

    using CPSNavigation to list folder contents. getNavigationItems.py: - added start and end depth parameters - added a 'contextual' option
    XXX TODO: show documents as well

--- skins/cpsportlets_widgets_cps3/getNavigationItems.py
+++ skins/cpsportlets_widgets_cps3/getNavigationItems.py
@@ -8,14 +8,26 @@ else:
     REQUEST=context.REQUEST
 
 context_rpath = kw.get('prefix')
-# XXX: root_uid
-nav = CPSNavigation(context_uid=context_rpath,
-                    context=context_obj,
-                    request_form=REQUEST.form,
-                    **kw
-                   )
+try:
+    nav = CPSNavigation(context_uid=context_rpath,
+                        context=context_obj,
+                        request_form=REQUEST.form,
+                        **kw)
+# root_uid not set
+except KeyError:
+    return []
 
 folder_items = []
+
+start_depth = kw.get('start_depth', 0)
+end_depth = kw.get('end_depth', 0)
+contextual = kw.get('contextual')
+
+# the depth is relative to the current folder in contextual mode
+delta = 0
+if contextual:
+    delta = len(context_rpath.split('/')) -1
+
 for tree in nav.getTree():
     object = tree['object']
 
@@ -23,8 +35,18 @@ for tree in nav.getTree():
     if only_children and object['rpath'] == context_rpath:
         continue
 
+    # filter out items outside the specified depth
+    depth = object['depth'] - delta
+    if start_depth:
+        if depth < start_depth:
+            continue
+    if end_depth:
+        if depth > end_depth:
+            continue
+
     folder_items.append({'url': object['url'],
                          'title': object['title_or_id'],
+                         'depth': depth,
                         })
 
 return folder_items
