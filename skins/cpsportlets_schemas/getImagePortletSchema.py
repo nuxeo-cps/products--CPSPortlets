2f49d65d9ea6b89dd4782e028479c07f41696456
jmorliaguet <devnull@localhost>
2005-02-15 15:40:34 +0000

    the image link widget used in the Image Portlet and in the Rotating Image Portlet displays an image caption (optional).

--- skins/cpsportlets_schemas/getImagePortletSchema.py
+++ skins/cpsportlets_schemas/getImagePortletSchema.py
@@ -35,6 +35,24 @@ image_portlet_schema = {
             'write_process_expr': '',
         },
     },
+    'caption': {
+        'type': 'CPS String Field',
+        'data': {
+            'default_expr': 'string:',
+            'is_searchabletext': 0,
+            'acl_read_permissions': '',
+            'acl_read_roles': '',
+            'acl_read_expr': '',
+            'acl_write_permissions': '',
+            'acl_write_roles': '',
+            'acl_write_expr': '',
+            'read_ignore_storage': 0,
+            'read_process_expr': '',
+            'read_process_dependent_fields': [],
+            'write_ignore_storage': 0,
+            'write_process_expr': '',
+        },
+    },
 }
 
 schemas = {'image_portlet': image_portlet_schema}
