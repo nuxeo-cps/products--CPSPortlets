46be3289f46040e3d20e985fde2360178873a18f
jmorliaguet <devnull@localhost>
2005-02-25 23:34:14 +0000

    Internal Links Portlet: new rendering method in view mode with more configuration options and optimized rendering of icon tags. Uses the same HTML markup as other portlets for CPSSkins styling.

---
+++ skins/cpsportlets_widgets/getItemsFromPath.py
@@ -0,0 +1,41 @@
+##parameters=links=[], max_title_words=0, **kw
+
+# XXX get base url from the request
+base_url = context.getBaseUrl()
+
+max_title_words = int(max_title_words)
+
+portal_url = context.portal_url
+portal = portal_url.getPortalObject()
+
+items = []
+mtool = context.portal_membership
+checkPerm = mtool.checkPermission
+portal_types = context.portal_types
+renderIcon = context.portal_cpsportlets.renderIcon
+
+for path in links:
+    if path.startswith('/'):
+        path = path[1:]
+    object = portal.restrictedTraverse(path, default=None)
+    if object is None:
+        continue
+    if not checkPerm('View', object):
+        continue
+
+    ptype = getattr(object, 'portal_type', None)
+
+    # title 
+    title = object.title_or_id()
+    if max_title_words > 0:
+        words = title.split(' ')
+        if len(words) > max_title_words:
+            title = ' '.join(words[:int(max_title_words)]) + ' ...'
+
+    items.append(
+        {'url': '/' + object.absolute_url(relative=1),
+         'title': title,
+         'icon_tag': renderIcon(ptype, base_url, ''),
+        })
+
+return items
