e82ee4d14a46f456461f3217202edc7c41dba6db
jmorliaguet <devnull@localhost>
2005-04-05 21:26:42 +0000

    - Navigation portlet: added an 'Up to folder parent' display mode. - UI: Language Portlet added a submit button next to the select widget.

--- skins/cpsportlets_vocabularies/getNavigationPortletVocabulary.py
+++ skins/cpsportlets_vocabularies/getNavigationPortletVocabulary.py
@@ -1,18 +1,20 @@
 cpsportlets_navigation_display_voc_vocabulary = {
     'data': {
         'tuples': (
-            ('collapsible_menu', 'Collapsible menu',
-             'cpsportlets_nav_display_collapsible_menu_label'),
-            ('vertical_breadcrumbs_menu', 'Vertical breadcrumbs menu',
-             'cpsportlets_nav_display_vertical_breadcrumbs_menu_label'),
-            ('folder_contents', 'Folder contents',
-             'cpsportlets_nav_display_folder_contents_menu_label'),
-            ('subfolder_contents', 'Sub-folder contents',
-             'cpsportlets_nav_display_subfolder_contents_menu_label'),
             ('navigation_tree', 'Navigation tree',
              'cpsportlets_nav_display_navtree_menu_label'),
+            ('folder_contents', 'Folder contents',
+             'cpsportlets_nav_display_folder_contents_menu_label'),
+            ('collapsible_menu', 'Collapsible menu',
+             'cpsportlets_nav_display_collapsible_menu_label'),
             ('site_map', 'Site map',
              'cpsportlets_nav_display_site_map_label'),
+            ('vertical_breadcrumbs_menu', 'Vertical breadcrumbs menu',
+             'cpsportlets_nav_display_vertical_breadcrumbs_menu_label'),
+            ('subfolder_contents', 'Sub-folder contents',
+             'cpsportlets_nav_display_subfolder_contents_menu_label'),
+            ('up_to_parent', 'Up to parent folder',
+             'cpsportlets_nav_display_up_to_parent_label'),
         ),
     },
 }
