3fbeee36a2331fb6ed9e2e0406947e7ef38d44e4
ogrisel <devnull@localhost>
2005-06-06 12:50:22 +0000

    reset the Language Selection flag in SESSION in the language selection portlet

--- skins/cpsportlets_widgets_cps3/cpsportlet_change_language.py
+++ skins/cpsportlets_widgets_cps3/cpsportlet_change_language.py
@@ -6,10 +6,19 @@ if not lang:
 if REQUEST is None:
     REQUEST = context.REQUEST
 
+from Products.CPSCore.utils import resetSessionLanguageSelection
+
+# Reset previous language settings in the current session
+resetSessionLanguageSelection(REQUEST)
+
 # switch to 'lang'
 portal = context.portal_url.getPortalObject()
 portal.Localizer.changeLanguage(lang=lang)
 
 if REQUEST is not None:
     redirect_url = REQUEST['HTTP_REFERER']
+    # Avoid redirection to a switchLanguage action
+    index = redirect_url.find('/switchLanguage')
+    if index != -1:
+        redirect_url = redirect_url[:index]
     REQUEST.RESPONSE.redirect(redirect_url)
