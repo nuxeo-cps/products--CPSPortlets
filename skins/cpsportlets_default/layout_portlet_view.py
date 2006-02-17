10c35ed35aaa0c821e9f6e7c988dcdc5456b673e
tziade <devnull@localhost>
2006-02-17 13:38:11 +0000

    adding an id to target a portlet rendering on the page

--- skins/cpsportlets_default/layout_portlet_view.py
+++ skins/cpsportlets_default/layout_portlet_view.py
@@ -1,8 +1,11 @@
 ##parameters=layout=None, **kw
-
 rendered = []
 for row in layout['rows']:
     for cell in row:
         rendered.append(cell['widget_rendered'])
 
-return ''.join(rendered)
+result = ''.join(rendered)
+if result == '':
+    return ''
+
+return '<div id="%s">%s</div>' % (context.getId(), result)
