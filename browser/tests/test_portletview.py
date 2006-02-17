1c6e113399b5b51249a3cce191a99f96ddf7708c
tziade <devnull@localhost>
2006-02-17 14:29:37 +0000

    adding XHTML tests over portlets

--- browser/tests/test_portletview.py
+++ browser/tests/test_portletview.py
@@ -49,6 +49,41 @@ class PortletViewTests(CPSPortletsTestCase.CPSPortletsTestCase):
                 continue
             self.assert_(result.startswith('<div id="%s">' % portlet_id))
 
+    def testValidXHTML(self):
+        htmlpage = """
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+        <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+          <head>
+            <title>My Portlet, alone</title>
+          </head>
+          <body>
+          %s
+          </body>
+        </html>
+        """
+
+        # XXX portlets to be checked (img tag issue)
+        not_tested = ('portlet_language', 'portlet_text_about_cps',
+                      'portlet_text_cps', 'portlet_text_start_cps',
+                      'portlet_text_welcome3')
+
+        portlets = getToolByName(self.portal, 'portal_cpsportlets')
+
+        fakepage = ''
+        for portlet_id in portlets.objectIds():
+            if portlet_id in not_tested:
+                continue
+            portletviewer = PortletView(self.portal, None)
+            result = portletviewer.render(portlet_id)
+            if result.strip() == '':
+                continue
+            fakepage += '\n<div>**** portlet %s ****</div>\n%s\n\n' \
+                        % (portlet_id, result)
+
+        self.assertValidXHTML(htmlpage % fakepage)
+
 def test_suite():
     return unittest.TestSuite((
         unittest.makeSuite(PortletViewTests)
