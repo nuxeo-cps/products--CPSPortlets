1647de4c5e9161915a8a3353375c66735f9e0723
lregebro <devnull@localhost>
2006-04-05 19:17:08 +0000

    Now sorts the unordered actions per the ActionIcon priority.

--- skins/cpsportlets_widgets/getActionsItems.py
+++ skins/cpsportlets_widgets/getActionsItems.py
@@ -1,6 +1,11 @@
 ##parameters=obj=None, categories=[], actions_order=[]
 
+from Products.CMFCore.utils import getToolByName
+
+DEFAULT_PRIORITY = 8
+
 actions = context.REQUEST.get('cpsskins_cmfactions', None)
+
 if actions is None:
     actions = context.portal_actions.listFilteredActionsFor(obj)
 
@@ -10,9 +15,12 @@ utool = context.portal_url
 # Note: this will not work in CMF
 base_url = utool.getBaseUrl()
 
+atool = getToolByName(context, 'portal_actionicons', None)
+
 actionitems = []
 orderedActions = list(actions_order)
 reorderedActions = []
+unordereditems = []
 
 for category in categories:
     if not actions.has_key(category):
@@ -26,17 +34,27 @@ for category in categories:
                      base_url=base_url,
                      alt=action['name']),
                 }
-        # if action is not in the preordered list, append it as usual
+        item['priority'] = DEFAULT_PRIORITY
+        if atool is not None:
+            info = atool.queryActionInfo(category, action['id'])
+            if info is not None:
+                item['priority'] = info[1]
+                
         if not action['id'] in actions_order:
             actionitems.append(item)
         else:
             # replace the action id in the preordered list with the action 
             # vocabulary
             orderedActions[actions_order.index(action['id'])] = item
-
+        
 for action in orderedActions:
     # keep only actions that really exist (have been replaced with an item)
     if not isinstance(action, str):
         reorderedActions.append(action)
+
+# Sort the unsorted ones:
+actionitems.sort( lambda x, y: cmp( x.get( 'priority', 0 )
+                                       , y.get( 'priority', 0 )
+                                       ) )
 
 return reorderedActions + actionitems
