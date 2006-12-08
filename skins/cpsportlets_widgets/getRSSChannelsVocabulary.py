2b11b0319cb880de7370a21f7b86e6eea01b20c8
madarche <devnull@localhost>
2006-12-08 15:46:22 +0000

    - Fixed the fact that RSS channels without a title where difficult to understand   and select from the RSS Portlet.

--- skins/cpsportlets_widgets/getRSSChannelsVocabulary.py
+++ skins/cpsportlets_widgets/getRSSChannelsVocabulary.py
@@ -6,9 +6,9 @@ portal_rss = getattr(context, 'portal_rss', None)
 if portal_rss is not None:
     for channel in portal_rss.objectValues():
         channel_id = channel.getId()
-        channel_title = channel.Title()
+        channel_title_or_id = channel.title_or_id()
         if key is not None and key == channel_id:
-            return channel_title
-        items.append((channel_id, channel_title))
+            return channel_title_or_id
+        items.append((channel_id, channel_title_or_id))
 
 return items
