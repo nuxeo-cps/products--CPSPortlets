1e6079aa56be91e3d08aa00502c096579633a86f
madarche <devnull@localhost>
2008-01-12 21:57:43 +0000

    Fixed #1880: Some CPSPortlets code duplicates CPSCore.URLTool.getBreadCrumbsInfo.

--- skins/cpsportlets_widgets/getBreadcrumbsItems.py
+++ skins/cpsportlets_widgets/getBreadcrumbsItems.py
@@ -2,56 +2,24 @@
 # $Id$
 
 REQUEST = context.REQUEST
+# XXX: Document what the breadcrumb_set is for !
 breadcrumb_set = REQUEST.get('breadcrumb_set')
 if breadcrumb_set != None:
     return breadcrumb_set
 
 utool = context.portal_url
 
-# display options
+# Display options
 parent = int(kw.get('parent', 0))
 display_hidden_folders = int(kw.get('display_hidden_folders', 1))
 display_site_root = int(kw.get('display_site_root', 1))
 first_item = int(kw.get('first_item', 0))
 
-# compute the breadcrumbs
+# Compute the breadcrumbs
 base_url = utool.getBaseUrl()
 portal = utool.getPortalObject()
-breadcrumbs = utool.getBreadCrumbs(context=context, only_parents=parent,
-                                   restricted=True)
-
-items = []
-if first_item == 0 or display_site_root:
-    items.append(
-        {'id': portal.getId(),
-         'title': portal.title_or_id(),
-         'url': base_url,
-        })
-
-if first_item == 0:
-    first_item = 1
-
-for obj in breadcrumbs[first_item:]:
-    # compute the title
-    title = obj.title_or_id()
-    try:
-        is_archived = obj.isProxyArchived()
-    except AttributeError:
-        is_archived = 0
-    if is_archived:
-        # XXX i18n
-        title = 'v%s (%s)' % (obj.getRevision(), title)
-
-    # hidden folders
-    if not display_hidden_folders:
-        content = obj.getContent()
-        if getattr(content.aq_inner.aq_explicit, 'hidden_folder', 0):
-            continue
-
-    items.append({
-        'id': obj.getId(),
-        'title': title,
-        'url': obj.absolute_url_path(),
-        })
-
+items = utool.getBreadCrumbsInfo(context=context, only_parents=parent,
+                                 restricted=True,
+                                 show_hidden_folders=display_hidden_folders,
+                                 first_item=first_item)
 return items
