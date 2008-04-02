536b3f73e834cbdc5df4219dd66688107b4404f3
madarche <devnull@localhost>
2008-04-02 15:04:39 +0000

    Fixed #1900: It should be possible to set multiple RSS channels in a RSS portlet.

--- skins/cpsportlets_widgets/getRSSItems.py
+++ skins/cpsportlets_widgets/getRSSItems.py
@@ -1,29 +1,49 @@
 ##parameters=**kw
+#
+# $Id$
+"""The script that returns RSS items according to the portlet parameters.
+"""
+
+from logging import getLogger
+
+import operator
+
+LOG_KEY = 'CPSPortlets.getRSSItems'
+
+DEFAULT_RSS_ITEM_DISPLAY = 'cpsportlet_rssitem_display'
+
+logger = getLogger(LOG_KEY)
 
 rsstool = getattr(context, 'portal_rss', None)
 if rsstool is None:
-    return []
-
-channel_id = kw.get('channel')
-channel = getattr(rsstool.aq_inner.aq_explicit, channel_id, None)
-
-if channel is None:
     return []
 
 first_item = int(kw.get('first_item', 1))
 max_items = int(kw.get('max_items', 0))
 max_words = int(kw.get('max_words', 0))
 
-DEFAULT_RSS_ITEM_DISPLAY = 'cpsportlet_rssitem_display'
-render_method = kw.get('render_method') or DEFAULT_RSS_ITEM_DISPLAY
-render_method = getattr(context, render_method, None)
+data_items = []
+channels_ids = kw.get('channels')
+for channel_id in channels_ids:
+    channel = getattr(rsstool.aq_inner.aq_explicit, channel_id, None)
+    if channel is None:
+        continue
+    data = channel.getData(max_items + first_item - 1)
+    lines = data['lines']
+    data_items += lines
+    if first_item > 1:
+        data_items = data_items[first_item - 1:]
 
-data = channel.getData(max_items + first_item -1)
-items = []
+logger.debug("data_items = %s" % str(data_items))
 
-data_items = data['lines']
-if first_item > 1:
-    data_items = data_items[first_item-1:]
+# If there is more than 1 channel we need to sort the rss items to only keep the
+# more recent ones, up to max_items.
+if len(channels_ids) > 1:
+    # NOTE: One should replace "modified" with "updated" if switching to a newer
+    # version of Feed Parser http://feedparser.org/docs/date-parsing.html
+    # Relying on the 'modified_parsed' item for the sorting.
+    data_items.sort(key=operator.itemgetter('modified_parsed'), reverse=True)
+    data_items = data_items[:max_items]
 
 def summarize(text='', max_words=20):
     """summarize the text by returning the first max_words
@@ -37,6 +57,9 @@ def summarize(text='', max_words=20):
     if split_text:
         res = ' '.join(split_text) + ' ...'
     return res
+
+render_method = kw.get('render_method') or DEFAULT_RSS_ITEM_DISPLAY
+render_method = getattr(context, render_method, None)
 
 order = 0
 for item in data_items:
