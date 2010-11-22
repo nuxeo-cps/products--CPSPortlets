735940ee2b212181b3e0fe8359c4455d6c415092
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-11-22 16:33:08 +0100

    Walk based unicode upgrade (can't rely on catalog here)

--- upgrade.py
+++ upgrade.py
@@ -19,8 +19,12 @@
 
 import logging
 import transaction
+
 from Products.CMFCore.utils import getToolByName
+from Products.CPSCore.ProxyBase import walk_cps_folders
+
 from PortletsCatalogTool import reindex_portlets_catalog
+from PortletsContainer import PortletsContainer
 
 def upgrade_335_336_portlets_catalog(context):
     """Migrates the CPS Portlets indexes to portal_cpsportlets
@@ -214,23 +218,35 @@ def upgrade_unicode(portal):
     CPS String Field content will be cast to unicode, whereas
     CPS Ascii String Field content will be cast to str
     """
-
     logger = logging.getLogger('Products.CPSPortlets.upgrades.unicode')
-    ptool = portal.portal_cpsportlets
-
-    reindex_portlets_catalog(portal) # really need to ensure that
     logger.info("Starting upgrade of portlets")
-
-    ptls = ptool.listAllPortlets()
-    total = len(ptls)
-    logger.info("Starting, found %d portlets", total)
-    done = 0
-    for ptl in ptls:
-        if not upgrade_doc_unicode(ptl):
-            logger.error("Could not upgrade portlet %s", doc)
-            continue
-        done += 1
+    counters = dict(done=0, total=0)
+    for folder in walk_cps_folders(portal):
+        upgrade_unicode_in(folder, counters=counters)
+    upgrade_container_unicode(portal.portal_cpsportlets, counters=counters)
     transaction.commit()
-    logger.info("Upgraded %d/%d portlets.", done, total)
+    logger.warn(
+        "Finished to upgrade portlets to unicode. "
+        "Successful for %(done)d/%(total)d portlets", counters)
+    reindex_portlets_catalog(portal) # not sure
+    transaction.commit()
 
-    reindex_portlets_catalog(portal)
+def upgrade_unicode_in(folder, counters=None):
+    if counters is None:
+        counters = dict(done=0, total=0)
+    for container in folder.objectValues([PortletsContainer.meta_type]):
+        upgrade_container_unicode(container, counters=counters)
+
+def upgrade_container_unicode(container, counters=None):
+    """Upgrade the portlets from a container to unicode."""
+    logger = logging.getLogger('Products.CPSPortlets.upgrades.unicode')
+    if counters is None:
+        counters = dict(done=0, total=0)
+    for portlet in container.listPortlets():
+        counters['total'] += 1
+        if upgrade_doc_unicode(portlet):
+            counters['done'] += 1
+        if counters['done'] % 100 == 0:
+            logger.info("Upgraded workflow history for %d documents.",
+                        counters['done'])
+            transaction.commit()
