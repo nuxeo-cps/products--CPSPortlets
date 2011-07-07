42fb6c299eb98f988a6f0ae57383f9727df35651
Georges Racinet on ishtar.racinet.fr <georges@racinet.fr>
2011-07-07 13:34:08 +0200

    #2413: new force reindexing method

--- PortletsCatalogTool.py
+++ PortletsCatalogTool.py
@@ -19,12 +19,18 @@
 """Portlets Catalog Tool
 """
 
+import logging
+import transaction
 from Globals import InitializeClass
 from zope.interface import implements
 
 from Products.CMFCore.CatalogTool import CatalogTool
 from Products.CMFCore.utils import getToolByName
+from Products.CPSCore.ProxyBase import walk_cps_folders
 from Products.CPSPortlets.interfaces import IPortletCatalogTool
+from interfaces import IPortletContainer
+
+logger = logging.getLogger(__name__)
 
 class PortletsCatalogTool(CatalogTool):
     """Portlets Catalog Tool
@@ -43,6 +49,46 @@ class PortletsCatalogTool(CatalogTool):
         ptl_tool = getToolByName(self, 'portal_cpsportlets', None)
         if ptl_tool is not None:
             ptl_tool._invalidatePortletLookupCache()
+
+    def indexPortletsIn(self, folder):
+        """Index all portlets that are in the folder.
+
+        Return the number of portlets done.
+        """
+        if IPortletContainer.providedBy(folder):
+            container = folder
+        else:
+            ptltool = getToolByName(self, 'portal_cpsportlets')
+            container = ptltool.getPortletContainer(context=folder, local=True)
+            if container is None:
+                logger.debug("No portlet container in %s", folder)
+                return 0
+
+        logger.info("Indexing portlets in %s", container)
+        for ptl in container.listPortlets():
+            done += 1
+            self.catalog_object(ptl)
+
+        return done
+
+    def forceRecatalog(self, folder=None, pghandler=None):
+        if folder is None:
+            self.manage_catalogClear()
+            folder = getToolByName(self, 'portal_url').getPortalObject()
+
+        prev_threshold = done = 0
+
+        self.indexPortletsIn(folder)
+        for subf in walk_cps_folders(folder):
+            done += self.indexPortletsIn(subf)
+            if done >= prev_threshold:
+                logger.info("Indexed %d portlets")
+                prev_threshold = done / 100
+                transaction.savepoint()
+
+        logger.info("forceRecatalog done indexing a total of %d portlets",
+                    done)
+
 
 InitializeClass(PortletsCatalogTool)
 
